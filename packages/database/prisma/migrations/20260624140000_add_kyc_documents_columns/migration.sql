68c537e989cf351a14ddde3b26300422d60a9bbf
gokulmathan-git <gokulmathan21@gmail.com>
2026-06-24 15:05:05 +0530

    railway issue

---
+++ packages/database/prisma/migrations/20260624140000_add_kyc_documents_columns/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "aadhar_document_url" TEXT,
+ADD COLUMN     "pan_document_url" TEXT,
+ADD COLUMN     "gst_number" TEXT,
+ADD COLUMN     "status_reason" TEXT;
